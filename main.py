b7cf68b66bf37107a3b66cf550a2a3472932d09a
Lia Topalova <lia.topalova@gmail.com>
2023-10-16 13:45:36 +0300

    Implemented first task

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import re
 
+
+def validate_phone_number(number):
+    current_num = re.sub(r'\D', '', number)
+    pattern = r'^[0-9]{5,10}$'
+    if re.match(pattern, current_num):
+        return True
+    else:
+        return False
+
+
+print(validate_phone_number("0462 5-18-28"))

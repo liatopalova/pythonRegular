0b8817ebe66785a6929aafbc132a49d5fbb052dc
Lia Topalova <lia.topalova@gmail.com>
2023-10-16 13:47:08 +0300

    Implemented second task

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ def validate_phone_number(number):
 
 
 print(validate_phone_number("0462 5-18-28"))
+
+
+def validate_mobile_number(number):
+    pattern = r'^[+]{0,1}?\d{2}?[\s]?\(?\d{3}\)?[-\s]?\d{3}?[-\s]?\d{2}?[-\s]?\d{2}$'
+    if re.match(pattern, number):
+        return True
+    else:
+        return False
+
+
+print(validate_mobile_number("+38(093)519 67 78"))
